e5274582c8f38e4db5b9deb96e77f2ed8122055d
Kirbyyy81 <ashleyc.work27@gmail.com>
2025-09-29 18:23:04 +0800

    Add Interactive Get to Know Me Flip Cards

---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,13 @@
+@@ .. @@
+ import FloatingShapes from './FloatingShapes';
+ import CustomIllustration from './CustomIllustration';
++import GetToKnowMe from './GetToKnowMe';
+ import usePortfolioData from '../hooks/usePortfolioData';
+@@ .. @@
+         </motion.section>
+
++        {/* Get to Know Me Section */}
++        <GetToKnowMe />
++
+         {/* Projects Section */}
+         <motion.section
